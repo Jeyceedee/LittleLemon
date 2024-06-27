4ee1564ebac84832ea7f91b0351d54cec43d1c1d
Jeyceedee <70811916+Jeyceedee@users.noreply.github.com>
2024-06-28 01:08:14 +0800

    Add token-based authentication for table booking api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, MenuItemsView, SingleMenuItemView
-
+from rest_framework.authtoken.views import obtain_auth_token
 # router = DefaultRouter()
 # router.register(r'users', UserViewSet)
 
@@ -10,4 +10,5 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('items/', MenuItemsView.as_view()),
     path('items/<int:pk>', SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token)
 ]
